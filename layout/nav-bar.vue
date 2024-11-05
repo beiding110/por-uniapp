05f51781bc7bbe3fe35c1614ed1705529c0548e6
杨子豪 <beiding110@gmail.com>
2024-11-05 15:16:57 +0800

    【修改】调整nav-bar组件slot插槽编写方式；

--- layout/nav-bar.vue
+++ layout/nav-bar.vue
@@ -1,76 +1,80 @@
 <template>
-	<view class="layout-nav-bar">
-		<view 
-		v-if="navbar"
-		class="nav-bar"
-		>
-			<u-navbar
-				:title="title"
-				autoBack
-				:bgColor="navBgColor"
-				:fixed="false"
-			>
-				<slot name="left" slot="left"></slot>
-				<slot name="right" slot="right"></slot>
-			</u-navbar>
-		</view>
-		
-		<view class="layout-content">
-			<slot></slot>
-		</view>
-		
-		<view 
-		v-if="$slots.foot"
-		class="foot-bar"
-		>
-			<slot name="foot"></slot>
-			
-			<view class="u-safe-bottom u-safe-area-inset-bottom"></view>
-		</view>
-		
-		<my-msg></my-msg>
-		
-		<!-- #ifdef H5 -->
-		<my-cnzz></my-cnzz>
-		<!-- #endif -->
-	</view>
+    <view class="layout-nav-bar">
+        <view v-if="navbar" class="nav-bar">
+            <u-navbar
+                :title="title"
+                :autoBack="!$slots.left"
+                :bgColor="navBgColor"
+                :fixed="false"
+            >
+                <template slot="left">
+                    <template v-if="$slots.left">
+                        <slot name="left"></slot>
+                    </template>
+
+                    <template v-else>
+                        <u-icon name="arrow-left" size="20"></u-icon>
+                    </template>
+                </template>
+
+                <template slot="right">
+                    <slot name="right"></slot>
+                </template>
+            </u-navbar>
+        </view>
+
+        <view class="layout-content">
+            <slot></slot>
+        </view>
+
+        <view v-if="$slots.foot" class="foot-bar">
+            <slot name="foot"></slot>
+
+            <view class="u-safe-bottom u-safe-area-inset-bottom"></view>
+        </view>
+
+        <my-msg></my-msg>
+
+        <!-- #ifdef H5 -->
+        <my-cnzz></my-cnzz>
+        <!-- #endif -->
+    </view>
 </template>
 
 <script>
-	export default {
-		props: {
-			navbar: {
-				type: Boolean,
-				default: true,
-			},
-			title: {
-				type: String,
-				default: '',
-			},
-			navBgColor: {
-				type: String,
-				default: '#ffffff',
-			},
-		},
-		methods: {
-			
-		}
-	};
+export default {
+    props: {
+        navbar: {
+            type: Boolean,
+            default: true,
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+        navBgColor: {
+            type: String,
+            default: '#ffffff',
+        },
+    },
+    methods: {},
+    mounted() {},
+};
 </script>
 
 <style scoped lang="scss">
-	.layout-nav-bar{
-		display: flex;
-		flex-direction: column;
-		height: 100%;
-		
-		.layout-content{
-			flex: 1;
-			overflow: auto;
-		}
-		
-		.foot-bar{
-			background: white;
-		}
-	}
+.layout-nav-bar {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .layout-content {
+        flex: 1;
+        overflow: auto;
+    }
+
+    .foot-bar {
+        background: white;
+    }
+}
 </style>
